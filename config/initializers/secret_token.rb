cfacc200c4ce8b2df30d384b0b71293ad846c0c5
DukeFruitarian <DukeFruitarian@gmail.com>
2013-04-20 22:10:35 +0700

    Initialize Genealogical tree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenealogicalTree::Application.config.secret_key_base = '02bbc85f17657eda80a24ab31f6f631a68cf1682409712ef2cd6552d01fc6f3e65df2b4860424585e213acca7f819dcfd3bfccd825bba6890eefadfa1698e0ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GenealogicalTree::Application.config.session_store :encrypted_cookie_store, key: '_genealogical_tree_session'
