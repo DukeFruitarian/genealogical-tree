24b8cfde6f2fee24f9f9c606ab4cead01ea47a0c
DukeFruitarian <DukeFruitarian@gmail.com>
2013-04-25 09:11:52 +0700

    Add more devise configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 GenealogicalTree::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'authentication', controller: 'sessions',path_names: {sign_up: 'sign'}
   resources :products
 
+  get ''
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
