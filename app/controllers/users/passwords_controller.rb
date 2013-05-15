c5db3f6e0bc0519bd1209ae91a7aa88e158b55cb
DukeFruitarian <DukeFruitarian@gmail.com>
2013-05-15 11:02:19 +0700

    Skip filter

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,9 @@
 class Users::PasswordsController < Devise::PasswordsController
+  skip_before_filter :auth
   def resource_params
     params.require(:user).permit(:email, :password, :password_confirmation, :reset_password_token)
   end
   private :resource_params
+
 end
+
